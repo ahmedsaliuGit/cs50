c551895df25a0502f5ac374f7947586cd6eb6122
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-21 11:22:19 +0000

    Fri, Jun 21, 2024, 12:22 PM +01:00

--- twttr.py
+++ twttr.py
@@ -1,6 +1,12 @@
 output = ""
+vowels = {"A": True, "E": True, "I": True, "O": True, "U": True, "a": True, "e": True, "i": True, "o": True, "u": True}
 
 answer = input("Input: ")
 
 for char in answer:
-    
+    if vowels[char]:
+        output += ""
+    else:
+        output += char
+
+print("Output:", output)
