78cfaf52efe5e4b7c6da6e68a6339309a2a99da5
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-21 11:30:27 +0000

    Fri, Jun 21, 2024, 12:30 PM +01:00

--- twttr.py
+++ twttr.py
@@ -4,7 +4,7 @@ vowels = {"A": True, "E": True, "I": True, "O": True, "U": True, "a": True, "e":
 answer = input("Input: ")
 
 for char in answer:
-    if char == "A" or char == "E" or char == "I" or char == "O" or char == "U" "a": True, "e": True, "i": True, "o": True, "u":
+    if char == "A" or char == "E" or char == "I" or char == "O" or char == "U":
         output += ""
     else:
         output += char
