0141076fb900f342015d7dacdcca5c2f7d8a2cbf
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-21 11:02:26 +0000

    Fri, Jun 21, 2024, 12:02 PM +01:00

--- coke.py
+++ coke.py
@@ -12,7 +12,7 @@ def collect_coin():
         coin = int(input("Insert coin: "))
 
         if coin == 25 or coin == 10 or coin == 5:
-            coin_received = coin_received + coin
+            coin_received += coin
 
             if coin_received >= coke_price:
                 return coin_received
