48ba7506af8e66fade59913e88acab9896c3810a
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-22 14:33:26 +0000

    Sat, Jun 22, 2024, 3:33 PM +01:00

---
+++ nutrition/nutrition.py
@@ -0,0 +1,12 @@
+fruits = {
+    "Apple": 130,
+    "Avocado California": 50,
+    "Banana": 110,
+    "Cantaloupe": 50,
+    "Grapefruit": 60,
+    "Grapes": 90,
+    "Honeydew Melon": 50,
+    "Kiwifruit": 90,
+    "Lemon": 15,
+    "Lemon": 15,
+}
