b92b9aedbd8d2b67694a615b4461e90b00552e63
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-23 17:00:34 +0000

    Sun, Jun 23, 2024, 6:00 PM +01:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -10,7 +10,7 @@ def get_order(prompt):
         except EOFError:
             break
         else:
-            if item_name.title() in menu_items:
+            if item_name.title() not in menu_items:
                 total += menu_items[item_name.title()]
 
                 print("Total: ${:0.2f}".format(total))
