3bbfc590d40ae0bd7577cba5fd3d4ee287a6eee0
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-23 15:38:26 +0000

    Sun, Jun 23, 2024, 4:38 PM +01:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,11 +1,12 @@
 def main():
     print(get_fraction("Fraction: "))
+
 def get_fraction(prompt):
     while True:
         try:
             x_y = input(prompt).split("/")
-            print(len(x_y))
-            if len(x_y) < 2:
+
+            if len(x_y) != 2:
                 raise IndexError("List index out of range.")
 
             x = int(x_y[0])
