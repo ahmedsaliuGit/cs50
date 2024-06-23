8574bae44c2d2193f592cb3284b9c9f7bf3b1cef
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-23 18:12:32 +0000

    Sun, Jun 23, 2024, 7:12 PM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -3,10 +3,27 @@ def main():
 
 def parse_date(date_string):
     valid_month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    
+
     if "/" in date_string:
         month, day, year = date_string.split("/")
     else:
         month, day, year = date_string.split(" ")
         month = valid_month_names.index(month) + 1
         day = day.strip(",")
+
+    year = int(year)
+    month = int(month)
+    day = int(day)
+
+    if year < 1:  # https://en.wikipedia.org/wiki/Anno_Domini
+        raise ValueError
+
+    if not (0 < month < 13):
+        raise ValueError
+
+    if not (0 < day < 32):
+        raise ValueError
+
+    return f"{year:>04}-{month:>02}-{day:>02}"
+
+main()
