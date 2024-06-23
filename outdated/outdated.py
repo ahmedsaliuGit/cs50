6ea19b5fc5eb4d251409da33cb2836ea560a54b5
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-23 18:10:28 +0000

    Sun, Jun 23, 2024, 7:10 PM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -2,6 +2,8 @@ def main():
 
 
 def parse_date(date_string):
+    valid_month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    
     if "/" in date_string:
         month, day, year = date_string.split("/")
     else:
