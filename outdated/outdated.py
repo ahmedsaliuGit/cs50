3e8e95731fb3da0f46b0f30506dd6164648716fb
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-23 18:09:35 +0000

    Sun, Jun 23, 2024, 7:09 PM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -2,4 +2,9 @@ def main():
 
 
 def parse_date(date_string):
-    
+    if "/" in date_string:
+        month, day, year = date_string.split("/")
+    else:
+        month, day, year = date_string.split(" ")
+        month = valid_month_names.index(month) + 1
+        day = day.strip(",")
