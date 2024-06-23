f199327582e76ff9b33e8a8dd726575d245a5b69
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-23 16:59:01 +0000

    Sun, Jun 23, 2024, 5:59 PM +01:00

---
+++ grocery/grocery.py
@@ -0,0 +1,18 @@
+def main():
+    get_order("Item: ")
+
+def get_order(prompt):
+    items = []
+
+    while True:
+        try:
+            item_name = input(prompt)
+        except EOFError:
+            break
+        else:
+            if item_name.title() in menu_items:
+                total += menu_items[item_name.title()]
+
+                print("Total: ${:0.2f}".format(total))
+
+main()
