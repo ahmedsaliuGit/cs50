eb30113a5b24d216536f896af645b7d0dab53ac8
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-24 14:28:05 +0000

    Mon, Jun 24, 2024, 3:28 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -7,13 +7,15 @@ fonts = figlet.getFonts()
 
 cmd_argv = sys.argv
 
-if len(cmd_argv) != 0 or len(cmd_argv) != 2:
+if len(cmd_argv) != 0:
     sys.exit()
+elif len(cmd_argv[1:]) == 2:
+    if cmd_argv[1] != "-f" or cmd_argv[1] != "--font":
+        sys.exit("Error: flag is not available")
 
-if cmd_argv[1] != "-f" or cmd_argv[1] != "--font":
-    sys.exit("Error: flag is not available")
+    if cmd_argv[2] not in fonts:
+        sys.exit("Error: font does not exist.")
 
-if cmd_argv[2] not in fonts:
-    sys.exit("Error: font does not exist.")
+    
 
 
