fbd03e6fe87fdd48b29b540390392639fd2fc98e
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-24 17:02:57 +0000

    Mon, Jun 24, 2024, 6:02 PM +01:00

--- game/game.py
+++ game/game.py
@@ -1,5 +1,11 @@
 def main():
+    print(get_input("Level "))
 
 def get_input(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
 
 main()
