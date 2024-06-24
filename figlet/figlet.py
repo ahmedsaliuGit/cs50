7ca94b776f106eb2e7c8d3bbcd7d582c9b52e495
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-24 14:04:50 +0000

    Mon, Jun 24, 2024, 3:04 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,10 +1,11 @@
+import sys
 from pyfiglet import Figlet
 
 figlet = Figlet()
 
 fonts = figlet.getFonts()
 
-argv_len = len(argv)
+argv_len = len(sys.argv)
 
 if argv_len != 0 or argv_len != 2:
     argv.exist()
