3ae0a9ab90478076261e5c846c88fc108460c2f3
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-24 18:05:01 +0000

    Mon, Jun 24, 2024, 7:05 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -5,8 +5,17 @@ def main():
     ...
 
 
-def get_level():
-    ...
+def get_level(prompt):
+    while True:
+        try:
+            num = int(input(prompt))
+
+            if num <= 0:
+                raise ValueError
+        except ValueError:
+            pass
+        else:
+            return num
 
 
 def generate_integer(level):
