e0337b2b5e72edce027cd96361828a2b92592425
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-24 18:25:16 +0000

    Mon, Jun 24, 2024, 7:25 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,7 +2,44 @@ import random
 
 
 def main():
-    print(get_level("Level: "))
+    level = get_level("Level: ")
+    error = 0
+    score = 0
+
+    for _ in range(0, 10):
+        X = generate_integer(level)
+        Y = generate_integer(level)
+        
+        while True:
+            try:
+                temp = int(input(f"{X} + {Y} = "))
+                break
+            except ValueError:
+                print("EEE")
+                error += 1
+                break
+        if temp != X+Y:
+            print("EEE")
+            while True:
+                try:
+                    temp = int(input(f"{X} + {Y} = "))
+                except ValueError:
+                    print("EEE")
+                    error += 1
+                    break
+                else:
+                    if temp == X+Y:
+                        score += 1
+                        break
+                    error += 1
+                    print("EEE")
+                    if error == 2:
+                        print(f"{X} + {Y} = {X+Y}")
+                        error = 0
+                        break
+        else:
+            score += 1
+    print("Score:", score)
 
 
 def get_level(prompt):
