8915a9b535ba98d3e0d3a5608dec73b71cd10108
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-25 14:55:14 +0000

    Tue, Jun 25, 2024, 3:55 PM +01:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,3 +1,18 @@
 from plates import is_valid
 
+def test_all_lette():
+    assert is_valid("AA") == True
+    assert is_valid("a") == False
+    assert is_valid("A") == False
 
+def test_all_number():
+    assert is_valid("11") == False
+
+def test_letter_number_2_len():
+    assert is_valid("A1") == False
+    assert is_valid("1A") == False
+
+def test_letter_number():
+    assert is_valid("cs50") == True
+    assert is_valid("CS50") == True
+    assert is_valid("AAA222") == True
