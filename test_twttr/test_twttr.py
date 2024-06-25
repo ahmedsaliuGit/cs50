2e9cc1bc30e26c5e3a8d94b04e8ed407558fca87
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-25 14:10:33 +0000

    Tue, Jun 25, 2024, 3:10 PM +01:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -5,3 +5,9 @@ def test_empty_str():
 
 def test_argument():
     assert shorten("Twitter") == "Twttr"
+
+def test_lowercase():
+    assert shorten("baller") == "bllr"
+
+def test_uppercase():
+    assert shorten("BALLER") == "BLLR"
