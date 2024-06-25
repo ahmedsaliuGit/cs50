a1248003ca8468ef94bdcce775f48e353974ffd5
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-25 10:45:27 +0000

    Tue, Jun 25, 2024, 11:45 AM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,7 +2,7 @@ import sys
 import requests
 
 try:
-    if len(sys.argv) > 1 and (sys.argv[1].isnumeric() or float(sys.argv[1])):
+    if len(sys.argv) > 1 and float(sys.argv[1]):
 
         response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
