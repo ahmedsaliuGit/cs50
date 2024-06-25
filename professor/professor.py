428ffd860b963131995f23db7edf915f88f615e8
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-25 08:14:23 +0000

    Tue, Jun 25, 2024, 9:14 AM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -53,13 +53,14 @@ def get_level(prompt):
 
 
 def generate_integer(level):
-    if level == 1:
-        return random.randint(0, 9)
-    elif level == 2:
-        return random.randint(10, 99)
-    elif level == 3:
-        return (random.randint(100, 999))
-    else:
+    try:
+        if level == 1:
+            return random.randint(0, 9)
+        elif level == 2:
+            return random.randint(10, 99)
+        elif level == 3:
+            return (random.randint(100, 999))
+    except:
         raise ValueError
 
 
