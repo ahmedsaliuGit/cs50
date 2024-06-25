f263d2cf66a5ab39688873b6f6287fd41ed4a77e
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-25 14:48:06 +0000

    Tue, Jun 25, 2024, 3:48 PM +01:00

--- test_plates/plate.py
+++ test_plates/plate.py
@@ -1,10 +1,41 @@
+import string
+
 def main():
-    ...
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
 
 def is_valid(s):
-    ...
+    if len(s) < 2 or len(s) > 6:
+        return False
+    elif s.isdigit():
+        return False
+    elif " " in s:
+        return False
+    else:
+        digits = ''
 
+        for i in range(len(s)):
+            char = s[i]
+
+            if char in string.punctuation:
+                return False
+
+            if digits == '' and char.isnumeric():
+                digits += s[i:len(s)]
+
+                if digits[0] == '0':
+                    return False
+
+                if digits.isdigit():
+                    continue
+                else:
+                    return False
+
+    return True
 
 if __name__ == "__main__":
     main()
