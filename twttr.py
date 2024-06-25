e3e36f83e859335812d136a383501e138707859f
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-25 13:56:32 +0000

    Tue, Jun 25, 2024, 2:56 PM +01:00

--- twttr.py
+++ twttr.py
@@ -1,11 +1,20 @@
-output = ""
+def main():
+    answer = input("Input: ")
 
-answer = input("Input: ")
+    print("Output:", shorten)
 
-for char in answer:
-    if char.title() == "A" or char.title() == "E" or char.title() == "I" or char.title() == "O" or char.title() == "U":
-        output += ""
-    else:
-        output += char
 
-print("Output:", output)
+def shorten(word):
+    output = ""
+
+    for char in answer:
+        if char.title() == "A" or char.title() == "E" or char.title() == "I" or char.title() == "O" or char.title() == "U":
+            output += ""
+        else:
+            output += char
+
+    return output
+
+
+if __name__ == "__main__":
+    main()
