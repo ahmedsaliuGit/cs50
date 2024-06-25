d01663db77c427d7ca9db35f478e0ee9e2fd3bf2
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-25 09:34:25 +0000

    Tue, Jun 25, 2024, 10:34 AM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -3,8 +3,10 @@ import requests
 
 if len(sys.argv) > 1 or sys.argv[1].isnumeric():
     try:
-        ...
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+
+        bpi = response.json()
     except requests.RequestException:
-        ...
+        print("Network error")
 else:
     sys.exit("Missing command-line argument")
