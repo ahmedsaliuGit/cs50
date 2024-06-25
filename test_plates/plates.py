11723564b72f929b87a200cf39a905a1dfed4df3
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-25 15:03:28 +0000

    Tue, Jun 25, 2024, 4:03 PM +01:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -1,5 +1,3 @@
-import string
-
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
@@ -9,35 +7,15 @@ def main():
         print("Invalid")
         return False
 
-
 def is_valid(s):
-    if len(s) < 2 or len(s) > 6:
-        return False
-    elif s.isdigit():
-        return False
-    elif " " in s:
-        return False
-    else:
-        digits = ''
-
-        for i in range(len(s)):
-            char = s[i]
-
-            if char in string.punctuation:
-                return False
-
-            if digits == '' and char.isnumeric():
-                digits += s[i:len(s)]
-
-                if digits[0] == '0':
-                    return False
-
-                if digits.isdigit():
-                    continue
+    if len(s) <= 6 and len(s) >= 2 and s[0:2].isalpha() and s.isalnum():
+        for i in s:
+            if i.isdigit():
+                result = s.index(i)
+                if s[result:].isdigit() and int(i) != 0:
+                    return True
                 else:
                     return False
-
-    return True
-
-if __name__ == "__main__":
-    main()
+        return True
+    else:
+        return False
