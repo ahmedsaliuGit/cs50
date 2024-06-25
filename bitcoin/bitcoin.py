21899974e3e26fe4161162f70aa65141ddbd0ef5
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-25 09:43:40 +0000

    Tue, Jun 25, 2024, 10:43 AM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -6,8 +6,8 @@ if len(sys.argv) > 1 or sys.argv[1].isnumeric():
         response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
         bpi = response.json()
-
-        print("${:0.2f}}".format(sys.argv[1] * bpi["bpi"]["USD"]["rate_float"]))
+        #print(float(sys.argv[1]) * bpi["bpi"]["USD"]["rate_float"])
+        print("${:0.2f}".format(float(sys.argv[1]) * bpi["bpi"]["USD"]["rate_float"]))
     except requests.RequestException:
         print("Network error")
 else:
