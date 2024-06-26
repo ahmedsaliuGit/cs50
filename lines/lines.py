926e5cd36df1d0bb3a25371f4d17a9a7c607a518
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-26 09:05:44 +0000

    Wed, Jun 26, 2024, 10:05 AM +01:00

--- lines/lines.py
+++ lines/lines.py
@@ -7,7 +7,12 @@ def validate_input():
     size = len(sys.argv)
 
     if size != 1:
-        
+        if size < 1:
+            return "Too few command-line arguments"
+        elif size > 1:
+            return "Too many command-line arguments"
+
+    return True
 
 if __name__ == "__main__":
     main()
