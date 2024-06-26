84a9b51d5ebc7bffa384c9ab94971aa95dae44d7
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-26 09:47:22 +0000

    Wed, Jun 26, 2024, 10:47 AM +01:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,15 +1,19 @@
 import sys
 
 def main():
-    lines_code =
+    lines_code =0
 
     if validate_input() == True:
         try:
             with open(sys.argv[1]) as file:
                 for line in file:
-                    
+                    if not line.startswith("# ") or not line.startswith("#") or line == " ":
+                        lines_code += 1
+
         except FileNotFoundError:
             sys.exit("File does not exist")
+    else:
+        sys.exit(validate_input())
 
 def validate_input():
     size = len(sys.argv[1:])
