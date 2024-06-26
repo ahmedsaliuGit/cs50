5b8332ab868cbb82e13860c4ad381025b9092568
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-26 10:11:29 +0000

    Wed, Jun 26, 2024, 11:11 AM +01:00

--- lines/test_lines.py
+++ lines/test_lines.py
@@ -7,7 +7,16 @@ def main():
     test_not_python_file()
 
 def test_argument_is_one():
-    assert validate_input() 
+    assert validate_input("hello.txt") == True
+
+def test_too_few_arguments():
+    assert validate_input() == "Too few command-line arguments"
+
+def test_too_many_arguments():
+    assert validate_input() == "Too many command-line arguments"
+
+def test_not_python_file():
+    assert validate_input() == "Not a Python file"
 
 if __name__ == "__main__":
     main()
