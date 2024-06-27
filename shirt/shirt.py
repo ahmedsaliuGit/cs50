e8a2408c8e4a627e711d9fa601f3eb0742298a68
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-27 10:36:24 +0000

    Thu, Jun 27, 2024, 11:36 AM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -24,13 +24,6 @@ def main():
 def validate_input(arguments):
     size = len(arguments[1:])
 
-    jpg1 = arguments[1].lower().endswith("jpg")
-    jpg2 = arguments[2].lower().endswith("jpg")
-    jpeg1 = arguments[1].lower().endswith("jpeg")
-    jpeg2 = arguments[2].lower().endswith("jpeg")
-    png1 = arguments[1].lower().endswith("png")
-    png2 = arguments[2].lower().endswith("png")
-
     if size != 2:
         if size < 2:
             return "Too few command-line arguments"
@@ -38,7 +31,7 @@ def validate_input(arguments):
             return "Too many command-line arguments"
     elif not arguments[1].lower().endswith((".jpg", ".jpeg", ".png")) and not arguments[2].lower().endswith((".jpg", ".jpeg", ".png")):
         return "Invalid input"
-    elif jpg1 != jpg2 or jpeg1 != jpeg2 or png1 != png2:
+    elif os.path.split(arguments[1].lower())[1] != os.path.split(arguments[2].lower())[1]:
         return "Input and output have different extensions"
 
     return True
