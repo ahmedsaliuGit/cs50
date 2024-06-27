42cbc1947ffd42f5655e026b45056b7f7cfeea86
Ahmed Saliu <saliu.lito@gmail.com>
2024-06-27 08:43:50 +0000

    Thu, Jun 27, 2024, 9:43 AM +01:00

---
+++ shirt/shirt.py
@@ -0,0 +1,44 @@
+import sys
+import csv
+
+def main():
+    is_valid = validate_input(sys.argv)
+    contents = []
+
+    if is_valid == True:
+        try:
+            with open(sys.argv[1], "r") as file:
+                reader = csv.reader(file)
+
+                for row in reader:
+                    if row[0] == "name":
+                        continue
+                    last, first = row[0].split(", ")
+                    contents.append([first, last, row[1]])
+
+            with open(sys.argv[2], "w") as file:
+                writer = csv.writer(file, delimiter=",")
+                writer.writerow(["first", "last", "house"])
+                writer.writerows(contents)
+        except IOError:
+            sys.exit(f"Could not read {sys.argv[1]}")
+    else:
+        sys.exit(is_valid)
+
+    # print(contents)
+
+def validate_input(arguments):
+    size = len(arguments[1:])
+
+    if size != 2:
+        if size < 2:
+            return "Too few command-line arguments"
+        elif size > 2:
+            return "Too many command-line arguments"
+    elif not arguments[1].endswith(".csv"):
+        return "Not a Python file"
+
+    return True
+
+if __name__ == "__main__":
+    main()
