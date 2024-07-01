ee6d41771016f682fc49768bee0e54f630fee53d
Ahmed Saliu <saliu.lito@gmail.com>
2024-07-01 14:52:30 +0000

    Mon, Jul 1, 2024, 3:52 PM +01:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -9,8 +9,8 @@ def main():
 def validate(ip):
     if matches := re.search(r"^(\d{1,3})\.(\d{1,3})\.\d{1,3}\.\d{1,3}$", ip):
         print(matches[1])
-        # if 0 < int(matches.group(0)) > 255 or 0 < int(matches.group(1)) > 255:
-        #     return False
+        if 0 < int(matches[1]) > 255 or 0 < int(matches[2]) > 255:
+            return False
 
         return "Valid"
     else:
