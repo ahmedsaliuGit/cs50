b0fc8db79deb7ea498da6fa51ca580832eb3e21b
Ahmed Saliu <saliu.lito@gmail.com>
2024-07-01 15:07:23 +0000

    Mon, Jul 1, 2024, 4:07 PM +01:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -5,17 +5,17 @@ def main():
     test_invalid_ip()
 
 def test_valid_ip():
-    assert validate("192.0.1.2") == "True"
-    assert validate("192.255.1.2") == "True"
-    assert validate("192.245.106.2") == "True"
-    assert validate("192.235.109.200") == "True"
+    assert validate("192.0.1.2") == True
+    assert validate("192.255.1.2") == True
+    assert validate("192.245.106.2") == True
+    assert validate("192.235.109.200") == True
 
 def test_invalid_ip():
-    assert validate("-192.0.1.2") == "False"
-    assert validate("cat") == "False"
-    assert validate("192.678.1.2") == "False"
-    assert validate("1234567890") == "False"
-    assert validate("-192.0.1") == "False"
+    assert validate("-192.0.1.2") == False
+    assert validate("cat") == False
+    assert validate("192.678.1.2") == False
+    assert validate("1234567890") == False
+    assert validate("-192.0.1") == False
 
 if __name__ == "__main__":
     main()
