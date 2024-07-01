09f2c6887f571668dbd8e8d3d571a4501712bc90
Ahmed Saliu <saliu.lito@gmail.com>
2024-07-01 14:19:06 +0000

    Mon, Jul 1, 2024, 3:19 PM +01:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -3,11 +3,14 @@ import sys
 
 
 def main():
-    print(validate(input("IPv4 Address: ")))
+    validate(input("IPv4 Address: "))
 
 
 def validate(ip):
-    if re.search(r"^\d{1,3}", ip)
+    if re.search(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", ip):
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
