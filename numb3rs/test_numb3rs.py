9807f7bbafd6948323ad5dfcc5cf399cd02ed74e
Ahmed Saliu <saliu.lito@gmail.com>
2024-07-01 15:04:43 +0000

    Mon, Jul 1, 2024, 4:04 PM +01:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -15,6 +15,7 @@ def test_invalid_ip():
     assert validate("cat") == "Invalid"
     assert validate("192.678.1.2") == "Invalid"
     assert validate("1234567890") == "Invalid"
+    assert validate("-192.0.1") == "Invalid"
 
 if __name__ == "__main__":
     main()
