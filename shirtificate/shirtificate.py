83a9358f32b15faf938ce84eb2394b321b946aef
Ahmed Saliu <saliu.lito@gmail.com>
2024-07-04 10:30:14 +0000

    Thu, Jul 4, 2024, 11:30 AM +01:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,8 +1,7 @@
 from fpdf import FPDF
 
 class PDF(FPDF):
-    def __init__(self):
-        super()
+    def header(self):
         self.image("./shirtificate.png", 10, 70, 190)
 
 def main():
@@ -11,15 +10,12 @@ def main():
 
 
 def shirt(s):
-    try:
-        pdf = PDF()
-        pdf.add_page()
-        pdf.set_font("helvetica", size=24)
-        pdf.set_text_color(255,255,255)
-        pdf.cell(0, 213, f"{s} took CS50", align="C")
-        pdf.output("shirtificate.pdf")
-    except RuntimeError as e:
-        print(e)
+    pdf = PDF()
+    pdf.add_page()
+    pdf.set_font("helvetica", size=24)
+    pdf.set_text_color(255,255,255)
+    pdf.cell(0, 213, f"{s} took CS50", align="C")
+    pdf.output("shirtificate.pdf")
 
 
 if __name__ == "__main__":
