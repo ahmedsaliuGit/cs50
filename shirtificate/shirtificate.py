7242b190f11fa132901376c66b6af44c0cc845ae
Ahmed Saliu <saliu.lito@gmail.com>
2024-07-04 10:04:08 +0000

    Thu, Jul 4, 2024, 11:04 AM +01:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -14,12 +14,15 @@ def main():
 
 
 def shirt(s):
-    pdf = PDF()
-    pdf.add_page()
-    pdf.set_font("helvetica", size=24)
-    pdf.set_text_color(255,255,255)
-    pdf.cell(0, 213, f"{s} took CS50", align="C")
-    pdf.output("shirtificate.pdf")
+    try:
+        pdf = PDF()
+        pdf.add_page()
+        pdf.set_font("helvetica", size=24)
+        pdf.set_text_color(255,255,255)
+        pdf.cell(0, 213, f"{s} took CS50", align="C")
+        pdf.output("shirtificate.pdf")
+    except RuntimeError as e:
+        print(e)
 
 
 if __name__ == "__main__":
