f6db95c406e9f37bb858175e66e299b45dbb4742
Ahmed Saliu <saliu.lito@gmail.com>
2024-07-04 10:06:18 +0000

    Thu, Jul 4, 2024, 11:06 AM +01:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,10 +3,6 @@ from fpdf import FPDF
 class PDF(FPDF):
     def header(self):
         self.image("./shirtificate.png", 10, 70, 190)
-        self.set_font("helvetica", "", 48)
-        self.cell(0, 57, "CS50 Shirtificate", align="C")
-        self.ln(20)
-
 
 def main():
     name = input("Name: ")
