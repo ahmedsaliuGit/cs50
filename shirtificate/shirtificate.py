33435ed9bee57d39d89e445d95b21eb8e36752cf
Ahmed Saliu <saliu.lito@gmail.com>
2024-07-04 09:54:19 +0000

    Thu, Jul 4, 2024, 10:54 AM +01:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,7 +3,7 @@ from fpdf import FPDF
 class PDF(FPDF):
     def header(self):
         self.image("./shirtificate.png", 10, 70, 190)
-        self.set_font("Ga Maamli", "", 48)
+        self.set_font("Roboto", "", 48)
         self.cell(0, 57, "CS50 Shirtificate", align="C")
         self.ln(20)
 
@@ -16,7 +16,7 @@ def main():
 def shirt(s):
     pdf = PDF()
     pdf.add_page(orientation="portrait", format="a4")
-    pdf.set_font("helvetica", size=24)
+    pdf.set_font("Roboto", size=24)
     pdf.set_text_color(255,255,255)
     pdf.cell(0, 213, f"{s} took CS50", align="C")
     pdf.output("shirtificate.pdf")
